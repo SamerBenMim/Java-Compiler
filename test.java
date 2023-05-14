b4b7e35a62bf427589d5e28529740fd11b41f6dd
SamerBenMim <samer.benmim@insat.ucar.tn>
2023-05-14 07:11:20 +0100

    add java test file

---
+++ test.java
@@ -0,0 +1,23 @@
+class SSACompiler {
+
+    double percentage(  int a , int b){
+	int x = 100;
+	return a / b * x ; 
+}
+	
+     double percentage(  int a , int b){
+		return 0 ;
+	}	
+
+    public static void main(String[] args) {
+	System.out.println("Hello, World!"); 
+	int y ;
+	int x = 2 ;	
+	y = 10 ;
+	double res = 0 ;
+	// percentage( 3) ; 
+	if ( 4 > 1 )  return 0 ; 
+	
+    }
+
+}
